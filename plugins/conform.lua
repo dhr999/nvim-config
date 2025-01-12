9dc9ea4930f9c5b0f879da4efb616c55eba0c095
dhr999 <dhruvthakkar61@gmail.com>
2025-01-12 11:53:38 +0000

    Linux config

---
+++ plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        cpp = { "prettier"},
+      }
+    })
+  end,
+}
